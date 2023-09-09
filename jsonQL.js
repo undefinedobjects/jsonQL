4190533257e24d0e65dc84e0b401447fd532a0c1
Mustafa UZUN <mustafauzun0@gmail.com>
2023-09-09 13:35:21 +0300

    fix: remove logs

--- jsonQL.js
+++ jsonQL.js
@@ -87,15 +87,3 @@ function jsonQL(jsonObject, stringPath) {
 
     return parser(jsonObject, lexer(stringPath));
 }
-
-let obj = {say: { message: 'hello' }, say2: { message: 'hello2' }};
-
-console.log(jsonQL(obj, 'say.message'));
-console.log(...jsonQL(obj, '<say.*>.message'));
-console.log(jsonQL(obj, '<say.*>.[1]'));
-console.log(jsonQL(obj, '<say.*>.[1](this.message == "hello2")'));
-console.log(jsonQL(obj, 'say.message'));
-console.log(jsonQL(obj, '<say.*>.[0].[0].(this)'));
-console.log(jsonQL(obj, '<say.*>.[0].message(this)'));
-console.log(jsonQL(obj, '<say.*>.[0].message'));
-console.log(jsonQL(obj, '<say.*>.[0].message()'));
